6bc42e94dc73651562d4aa371e8faef6de961222
Bill Herrick <wherrickjr@gmail.com>
2023-01-23 10:51:55 -0600

    finishing up

--- acquire.py
+++ acquire.py
@@ -8,8 +8,12 @@ import env
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectKBest, f_regression, RFE
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, TweedieRegressor
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LassoLars
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 
 def get_connection(db, user=env.username, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
@@ -94,3 +98,46 @@ def scale(df):
 # This reassigns the scaled versions into the dataframe by replacing the old numbers
     df[[ 'sqft', 'bedrooms', 'bathrooms']] = mms.fit_transform(df[[ 'sqft', 'bedrooms', 'bathrooms']])
     return df
+
+
+def ols(df):
+    x_train = df.drop(columns = 'tax_value')
+    y_train = df['tax_value']
+    lm = LinearRegression()
+    lm.fit(x_train, y_train)
+    lm_preds = lm.predict(x_train)
+
+    lasso = LassoLars(alpha = 0.05)
+    lasso.fit(x_train, y_train)
+    lasso_preds = lasso.predict(x_train)
+
+    pf = PolynomialFeatures(degree = 2)
+    pf.fit(x_train, y_train)
+    x_polynomial = pf.transform(x_train)
+    lmtwo = LinearRegression()
+    lmtwo.fit(x_polynomial, y_train)
+    poly_preds = lmtwo.predict(x_polynomial)
+
+    glm = TweedieRegressor(power=1, alpha=0)
+    glm.fit(x_train, y_train)
+    tweedie_preds = glm.predict(x_train)
+
+    all_models = pd.DataFrame()
+    all_models['actual'] = y_train
+    all_models['lm_preds'] = lm_preds
+    all_models['lasso_preds'] = lasso_preds
+    all_models['poly_preds'] = poly_preds
+    all_models['tweedie_preds'] = tweedie_preds
+    all_models['baseline'] = 397784
+
+    return all_models
+
+
+
+def calc_rmse(df):
+    lm_rmse = sqrt(mean_squared_error(df['actual'], df['lm_preds']))
+    lasso_rmse = sqrt(mean_squared_error(df['actual'], df['lasso_preds']))
+    poly_rmse = sqrt(mean_squared_error(df['actual'], df['poly_preds']))
+    tweedie_rmse = sqrt(mean_squared_error(df['actual'], df['tweedie_preds']))
+    base_rmse = sqrt(mean_squared_error(df['actual'], df['baseline']))
+    return pd.DataFrame({'lm_rmse': [lm_rmse], 'lasso_rmse': [lasso_rmse], 'poly_rmse': [poly_rmse], 'tweedie_rmse': [tweedie_rmse], 'base_rmse': [base_rmse]})
